0a066fb8562a487a1c048da67ac74794a159bdee
DanielMorales1103 <demoralesu@morurgt.com>
2025-05-15 18:40:18 -0600

    prueba

--- main.py
+++ main.py
@@ -1,30 +1,40 @@
-usuarios = []
+import os
+import subprocess
 
-def agregar_usuario(nombre, edad):
-    if edad < 0:
-        return  
+# Función larga con código duplicado
+def procesar_datos(archivo):
+    file = open("archivo.xlmx", "r")  
+    datos = file.readlines()
+    file.close()
 
-    user = {"nombre": nombre, "edad": edad}
-    usuarios.append(user)
+    resultados = []
+    for linea in datos:
+        partes = linea.strip().split(",")
+        if len(partes) > 1:
+            resultados.append(partes[1])
+        if len(partes) > 1:  
+            resultados.append(partes[1]) 
+    return resultados
 
-def eliminar_usuario(nombre):
-    for i in range(len(usuarios)):
-        if usuarios[i]["nombre"] == nombre:
-            del usuarios[i]  
-            return
-    else:
-        pass 
+usuario = "admin"  
 
-def imprimir_usuarios():
-    mensaje = ""  
-    for u in usuarios:
-        mensaje += u["nombre"] + ", edad: " + str(u["edad"]) + "\n"  
-    print(mensaje)
+def eliminar_archivo(path):
+    try:
+        os.remove(path)
+    except: 
+        print("Error")
 
-def main():
-    agregar_usuario("Ana", 22)
-    agregar_usuario("Luis", -3) 
-    eliminar_usuario(None)  
-    imprimir_usuarios()
+def esta_vacio(lista):
+    if len(lista) == 0:  
+        return True
+    return False
 
-main()
+def ejecutar_comando(comando):
+    resultado = subprocess.call("echo " + comando, shell=True)  
+    return resultado
+
+def no_se_usa():
+    pass  
+
+def funcion1(a, b):
+    return a + b * 2 - a / b + b - a + (b * 2) / a  
